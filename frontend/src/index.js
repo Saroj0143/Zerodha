bddf5ee3383acd23ad4adbebb9758129a318ba8b
Saroj0143 <sarojmohanty201819@gmail.com>
2026-09-06 23:27:27 +0530

    File changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <h1>Hello!</h1>
+      <h1>Hello! There</h1>
     </div>
   </React.StrictMode>
 );
