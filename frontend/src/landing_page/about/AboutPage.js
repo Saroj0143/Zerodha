92958541ce16ba89116abcf68f589affd03a6f37
Saroj0143 <sarojmohanty201819@gmail.com>
2026-09-17 15:32:59 +0530

    Router setup completed

--- frontend/src/landing_page/about/AboutPage.js
+++ frontend/src/landing_page/about/AboutPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Hero from '../home/Hero';
+import Team from './Team';
 
 
 function AboutPage() {
-    return ( 
-        <h1>AboutPage</h1>
-     );
+    return (
+        <>
+            <Hero />
+            <Team />
+        </>
+    );
 }
 
 export default AboutPage;
