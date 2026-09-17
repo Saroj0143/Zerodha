df8212af6d95c229c36ac12c55f23b48b6a8486a
Saroj0143 <sarojmohanty201819@gmail.com>
2026-09-17 13:45:37 +0530

    Navbar components setup

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,9 +1,114 @@
 import React from 'react';
 
 function Footer() {
-    return ( 
-        <h1>Footer</h1>
-     );
+    return (
+        <footer style={{ backgroundColor: "rgb(254, 249, 249)" }}>
+            <div className='container border-top mt-5'>
+                <div className='row mt-5'>
+                    <div className='col'>
+                        <img src='media/images/all-image/logo.svg' style={{ width: "50%" }} />
+                        <p style={{ fontSize: "14px" }}>© 2010 - 2026, Zerodha Broking Ltd.All rights reserved.</p>
+                    </div>
+                    <div className='col'>
+                        <p>Account</p>
+                        <a href='' style={{ textDecoration: "none" }}>Open demat account</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Minor demat account</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>NRI demat account</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>HUF demat account</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Commodity</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Dematerialisation</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Fund transfer</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>MTF</a>
+                        <br></br><br></br>
+                    </div>
+                    <div className='col'>
+                        <p>Support</p>
+                        <a href='' style={{ textDecoration: "none" }}>Contact us</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Support portal</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>How to file a complaint?</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Status of your complaints</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Bulletin</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Circular</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Z-Connect blog</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Downloads</a>
+                        <br></br><br></br>
+                    </div>
+                    <div className='col'>
+                        <p>Company</p>
+                        <a href='' style={{ textDecoration: "none" }}>About</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Products</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Pricing</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Referral programme</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Careers</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Zerodha.tech</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Press & media</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Zerodha cares (CSR)</a>
+                        <br></br><br></br>
+                    </div>
+                    <div className='col'>
+                        <p>Quick Links</p>
+                        <a href='' style={{ textDecoration: "none" }}>Upcoming IPOs</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Brokerage charges</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Market holidays</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Economic calendar</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Calculators</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Markets</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Sectors</a>
+                        <br></br><br></br>
+                        <a href='' style={{ textDecoration: "none" }}>Gift Nifty </a>
+                        <br></br><br></br>
+                    </div>
+                </div>
+                <div className='mt-5 text-muted' style={{ fontSize: "12px" }}>
+                    <p>Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to <a href='mailto:complaints@zerodha.com' style={{ textDecoration: "none" }}>complaints@zerodha.com</a>, for DP related to <a href='mailto:dp@zerodha.com' style={{ textDecoration: "none" }}> dp@zerodha.com</a>. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF</p>
+
+                    <p>Procedure to file a complaint on <a rel='nofollow' href='https://scores.sebi.gov.in/' target='_blank' style={{ textDecoration: "none" }}>SEBI SCORES</a>/<a rel='nofollow' href='https://smartodr.in/login' target='_blank' style={{ textDecoration: "none" }}>SMARTODR</a>: Register on SCORES portal & SMARTODR. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of grievances</p>
+
+                    <a rel='nofollow' href='https://smartodr.in/' target='_blank' style={{ textDecoration: "none" }}>Smart Online Dispute Resolution </a> | <a href="https://zerodha-common.s3.ap-south-1.amazonaws.com/Downloads-and-resources/Smart%20ODR%20info.pdf" target="_blank" style={{ textDecoration: "none" }}>Grievances Redressal Mechanism</a>
+
+                    <p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
+
+                    <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
+
+                    <p> India's largest broker based on networth as per NSE. <a rel="nofollow" href="https://enit.nseindia.com/MemDirWeb/brokerDetailPage_Beta?memID=2516&amp;h_MemType=members&amp;memName=ZERODHA%20BROKING%20LIMITED" target="_blank" style={{ textDecoration: "none" }}>NSE broker factsheet</a></p>
+
+                    <p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers/depository participants. Receive information of your transactions directly from Exchange/Depositories on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please <a href="https://support.zerodha.com/category/your-zerodha-account/your-profile/ticket-creation/articles/how-do-i-place-a-complaint-at-zerodha" style={{ textDecoration: "none" }}>create
+                        a ticket here</a></p>
+
+                    <p> *Customers availing insurance advisory services offered by Ditto (Tacterial Consulting Private Limited | IRDAI Registered Corporate Agent (Composite) License No CA0738) will not have access to the exchange investor grievance redressal forum, SEBI SCORES/ODR, or arbitration mechanism for such products.</p>
+
+                    <p>Fixed deposit products offered on this platform are third-party products (TPP) and are not Exchange traded products. These are offered through Blostem Fintech Private Limited. Zerodha Broking Limited (SEBI Registration No.: INZ000031633) is acting solely as a distributor for these products. Any disputes arising with respect to such distribution activity will not have access to SEBI SCORES/ODR, Exchange Investor Grievance Redressal Forum, or Arbitration mechanism. Fixed deposits are regulated by the Reserve Bank of India (RBI).</p>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
