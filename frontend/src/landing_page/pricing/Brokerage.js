de31c48377d1121ab1a3b3f915e89ecc8003d882
Saroj0143 <sarojmohanty201819@gmail.com>
2026-09-22 15:35:26 +0530

    pricing section completed

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
 
 function Brokerage() {
-    return ( 
-        <h1>Brokerage</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 text-center border-top">
+        <div className="col-8 p-4">
+          <a href="" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5">Brokerage Calculator</h3>
+          </a>
+          <ul style={{textAlign : "left", lineHeight : "2.5"}} className="text-muted fs-6">
+            <li>
+              Call & Trade and RMS auto-squareoff: Additional charges of 50 +GST per order.
+            </li>
+            <li>Digital contract notes will be sent via e-mail.</li>
+            <li>
+              Physical copies of contract notes, if required, shall be charged 20rs per contract note. Courier charges apply.
+            </li>
+            <li>
+              If the account is in debit balance, any order placed will be charges 50rs per executed order insted of 20rs per executed order.
+            </li>
+          </ul>
+        </div>
+        <div className="col-4 p-4">
+          <a href="" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5">List of Charges</h3>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Brokerage;
--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 border-bottom text-center">
+        <h2>Charges</h2>
+        <h3 className="text-muted mt-3 fs-5">List of all charges and taxes</h3>
+      </div>
+      <div className="row p-5 mt-5">
+        <div className="row text-center">
+          <div className="col">
+            <img
+              src="media/images/all-image/pricing0.svg"
+              style={{ width: "60%" }}
+            />
+            <h2 className="mt-3">Free equity delivery</h2>
+            <p className="text-muted mt-4">
+              All equity delivery investments (NSE, BSE), are absolutely free —
+              ₹ 0 brokerage.
+            </p>
+          </div>
+          <div className="col">
+            <img
+              src="media/images/all-image/intradayTrades.svg"
+              style={{ width: "60%" }}
+            />
+            <h2 className="mt-3">Intraday and F&O trades</h2>
+            <p className="text-muted mt-4">
+              Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
+              intraday trades across equity, currency, and commodity trades.
+              Flat ₹20 on all option trades.
+            </p>
+          </div>
+          <div className="col">
+            <img
+              src="media/images/all-image/pricing0.svg"
+              style={{ width: "60%" }}
+            />
+            <h2 className="mt-3">Free direct MF</h2>
+            <p className="text-muted mt-4">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
