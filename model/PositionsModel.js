9398456834cadebc847ee44ef47fe67c1992f554
Saroj Mohanty <sarojmohanty201819@gmail.com>
2026-09-23 22:58:50 +0530

    Delete model directory

--- model/PositionsModel.js
+++
@@ -1,7 +0,0 @@
-const {model} = require("mongoose");
-
-const {PositionsSchema} = require('../schemas/PositionsSchema');
-
-const PositionsModel = new model("Positions", PositionsSchema);
-
-module.exports = {PositionsModel};
