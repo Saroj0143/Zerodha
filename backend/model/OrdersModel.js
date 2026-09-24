18dfdaa405744d756eec3179d8dcadae1b5da136
Saroj Mohanty <sarojmohanty201819@gmail.com>
2026-09-24 21:26:24 +0530

    Delete backend directory

--- backend/model/OrdersModel.js
+++
@@ -1,7 +0,0 @@
-const {model} = require("mongoose");
-
-const {OrdersSchema} = require('../schemas/OrdersSchema');
-
-const OrdersModel = new model("Orders", OrdersSchema);
-
-module.exports = {OrdersModel};
--- backend/schemas/OrdersSchema.js
+++
@@ -1,7 +0,0 @@
-const {Schema} = require("mongoose");
-
-const OrdersSchema = new Schema({
-    
-});
-
-module.exports = {OrdersSchema};
